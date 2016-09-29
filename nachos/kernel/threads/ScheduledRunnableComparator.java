0505d7f1cfaa4babd207ffba7a7acb842e378011
Guanming Lin <guanming.lin@stonybrook.edu>
2016-09-29 17:02:40 -0400

    make Comparator a singleton

--- nachos/kernel/threads/ScheduledRunnableComparator.java
+++ nachos/kernel/threads/ScheduledRunnableComparator.java
@@ -4,9 +4,16 @@ import java.util.Comparator;
 
 public class ScheduledRunnableComparator implements Comparator<ScheduledRunnable> {
 
+    private static final class ScheduledRunnableComparatorInitializer {
+	private static final ScheduledRunnableComparator INSTANCE = new ScheduledRunnableComparator();
+    }
+    
     @Override
     public int compare(final ScheduledRunnable o1, final ScheduledRunnable o2) {
 	return Long.compare(o1.getScheduledTime(), o2.getScheduledTime());
     }
 
+    public static ScheduledRunnableComparator getInstance() {
+	return ScheduledRunnableComparatorInitializer.INSTANCE;
+    }
 }
