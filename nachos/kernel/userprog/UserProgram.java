b9b3422143568548751d9302cbae6df00dca8d52
Guanming Lin <guanming.lin@stonybrook.edu>
2016-10-22 01:10:36 -0400

    refactor ProgramTest

---
+++ nachos/kernel/userprog/UserProgram.java
@@ -0,0 +1,75 @@
+package nachos.kernel.userprog;
+
+import nachos.Debug;
+import nachos.kernel.Nachos;
+import nachos.kernel.filesys.OpenFile;
+import nachos.machine.CPU;
+import nachos.machine.NachosThread;
+
+public class UserProgram implements Runnable {
+
+    /** The name of the program to execute. */
+    private String execName;
+    private UserThread userThread;
+    
+    /**
+     * Start the test by creating a new address space and user thread,
+     * then arranging for the new thread to begin executing the run() method
+     * of this class.
+     *
+     * @param filename The name of the program to execute.
+     */
+    
+    public UserProgram(String filename) {
+	Debug.println('+', "starting ProgTest: " + filename);
+
+	execName = filename;
+	AddrSpace space = new AddrSpace();
+	userThread = new UserThread(filename, this, space);
+    }
+    
+    public UserProgram(String filename, int num) {
+	String name = "ProgTest"+ num + "(" + filename + ")";
+	
+	Debug.println('+', "starting ProgTest: " + name);
+
+	execName = filename;
+	AddrSpace space = new AddrSpace();
+	userThread = new UserThread(name, this, space);
+    }
+
+    /**
+     * Entry point for the thread created to run the user program.
+     * The specified executable file is used to initialize the address
+     * space for the current thread.  Once this has been done,
+     * CPU.run() is called to transfer control to user mode.
+     */
+    public void run() {
+	OpenFile executable;
+	System.out.println("running" + " " + execName);
+	if((executable = Nachos.fileSystem.open(execName)) == null) {
+	    Debug.println('+', "Unable to open executable file: " + execName);
+	    Nachos.scheduler.finishThread();
+	    return;
+	}
+
+	AddrSpace space = ((UserThread)NachosThread.currentThread()).space;
+	if(space.exec(executable) == -1) {
+	    Debug.println('+', "Unable to read executable file: " + execName);
+	    Nachos.scheduler.finishThread();
+	    return;
+	}
+
+	space.initRegisters();		// set the initial register values
+	space.restoreState();		// load page table register
+
+	CPU.runUserCode();			// jump to the user progam
+	Debug.ASSERT(false);		// machine->Run never returns;
+	// the address space exits
+	// by doing the syscall "exit"
+    }
+    
+    public void start() {
+	Nachos.scheduler.readyToRun(userThread);
+    }
+}
--- nachos/kernel/userprog/test/ProgTest.java
+++ nachos/kernel/userprog/test/ProgTest.java
@@ -16,6 +16,7 @@ import nachos.machine.CPU;
 import nachos.machine.NachosThread;
 import nachos.kernel.Nachos;
 import nachos.kernel.userprog.AddrSpace;
+import nachos.kernel.userprog.UserProgram;
 import nachos.kernel.userprog.UserThread;
 import nachos.kernel.filesys.OpenFile;
 
@@ -27,59 +28,7 @@ import nachos.kernel.filesys.OpenFile;
  * @author Peter Druschel (Rice University), Java translation
  * @author Eugene W. Stark (Stony Brook University)
  */
-public class ProgTest implements Runnable {
-
-    /** The name of the program to execute. */
-    private String execName;
-
-    /**
-     * Start the test by creating a new address space and user thread,
-     * then arranging for the new thread to begin executing the run() method
-     * of this class.
-     *
-     * @param filename The name of the program to execute.
-     */
-    public ProgTest(String filename, int num) {
-	String name = "ProgTest"+ num + "(" + filename + ")";
-	
-	Debug.println('+', "starting ProgTest: " + name);
-
-	execName = filename;
-	AddrSpace space = new AddrSpace();
-	UserThread t = new UserThread(name, this, space);
-	Nachos.scheduler.readyToRun(t);
-    }
-
-    /**
-     * Entry point for the thread created to run the user program.
-     * The specified executable file is used to initialize the address
-     * space for the current thread.  Once this has been done,
-     * CPU.run() is called to transfer control to user mode.
-     */
-    public void run() {
-	OpenFile executable;
-
-	if((executable = Nachos.fileSystem.open(execName)) == null) {
-	    Debug.println('+', "Unable to open executable file: " + execName);
-	    Nachos.scheduler.finishThread();
-	    return;
-	}
-
-	AddrSpace space = ((UserThread)NachosThread.currentThread()).space;
-	if(space.exec(executable) == -1) {
-	    Debug.println('+', "Unable to read executable file: " + execName);
-	    Nachos.scheduler.finishThread();
-	    return;
-	}
-
-	space.initRegisters();		// set the initial register values
-	space.restoreState();		// load page table register
-
-	CPU.runUserCode();			// jump to the user progam
-	Debug.ASSERT(false);		// machine->Run never returns;
-	// the address space exits
-	// by doing the syscall "exit"
-    }
+public class ProgTest {
 
     /**
      * Entry point for the test.  Command line arguments are checked for
@@ -98,7 +47,7 @@ public class ProgTest implements Runnable {
 				 "Usage: -x <executable file>",
 				 new Options.Action() {
 				    public void processOption(String flag, Object[] params) {
-					new ProgTest((String)params[0], count[0]++);
+					new UserProgram((String)params[0], count[0]++).start();
 				    }
 				 })
 		 });
