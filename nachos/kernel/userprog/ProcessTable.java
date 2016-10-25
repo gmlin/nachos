8be763d554784f1f499ccdc4980343ad0f852a6a
Guanming Lin <guanming.lin@stonybrook.edu>
2016-10-25 19:08:19 -0400

    fixed processtable

--- nachos/kernel/userprog/ProcessTable.java
+++ nachos/kernel/userprog/ProcessTable.java
@@ -9,7 +9,7 @@ import nachos.kernel.threads.Lock;
 public class ProcessTable {
 
     private int nextSpaceId;
-    private Map<AddrSpace, Integer> addrSpaceMap;
+    private Map<Integer, AddrSpace> addrSpaceMap;
     private Lock lock;
     
     public ProcessTable() {
@@ -18,29 +18,24 @@ public class ProcessTable {
 	lock = new Lock("Process table lock");
     }
     
-    /** returns the spaceId for an existing or new AddrSpace */
-    public int getOrAddSpace(AddrSpace space) {
+    /** returns the spaceId for a new process */
+    public int addProcess(AddrSpace space) {
 	int spaceId;
 	lock.acquire();
-	if (addrSpaceMap.containsKey(space))
-	    spaceId = addrSpaceMap.get(space);
-	else {
-	    spaceId = nextSpaceId++;
-	    addrSpaceMap.put(space, spaceId);
-	    Debug.println('0', "Added space " + spaceId);
-	}
+	spaceId = nextSpaceId++;
+	addrSpaceMap.put(spaceId, space);
+	Debug.println('0', "Added process " + spaceId);
 	lock.release();
 	
 	return spaceId;
     }
     
-    public void removeSpace(AddrSpace space) {
-	int spaceId;
+    public void removeProcess(int spaceId) {
 	lock.acquire();
-	spaceId = addrSpaceMap.remove(space);
+	addrSpaceMap.remove(spaceId);
 	lock.release();
 	
-	Debug.println('0', "Removed space " + spaceId);
+	Debug.println('0', "Removed process " + spaceId);
     }
     
 }
